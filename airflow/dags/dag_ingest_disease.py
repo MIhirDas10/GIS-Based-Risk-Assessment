b0188d8171ebb1a80694d181d18ffded17c2eefd
MIhirDas10 <dasmihir2911@gmail.com>
2026-03-13 02:26:48 +0600

    dag

--- airflow/dags/dag_ingest_disease.py
+++ airflow/dags/dag_ingest_disease.py
@@ -4,3 +4,27 @@ from airflow.operators.python import PythonOperator
 import sys
 
 # more will done 
+sys.path.append(0, "/opt/airflow/src")
+
+from ingestion.ingest_disease import run as ingest_disease
+
+default_args = {
+    "owner": "dengue_team",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id = "ingest_disease_data",
+    desc = "load kaggle dengue CSV into PostGIS",
+    start_date = datetime(2024, 1, 1),
+    schedule_interval = "@weekly",
+    catchup = False,
+    default_args = default_args,
+    tags = ["ingestion", "disease"],
+) as dag:
+    
+    ingest_task = PythonOperator(
+        task_id = "ingest_dengue_cases",
+        python_callable = ingest_disease,
+    )
